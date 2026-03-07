38bd7418dad8883a54b3dc9f2b63e9ab47794045
Saptarshi <babatezpur@gmail.com>
2026-03-08 01:09:12 +0530

    Add FloatingActionButton to HomeScreen

--- app/src/main/java/com/saptarshi/aipal/ui/home/HomeScreen.kt
+++ app/src/main/java/com/saptarshi/aipal/ui/home/HomeScreen.kt
@@ -6,13 +6,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -81,7 +84,13 @@ fun HomeScreenContent(viewModel: HomeViewModel) {
                     }
                 },
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { }) {
+                Icon(Icons.Filled.Add, "Floating action button.")
+            }
         }
+
     ) { innerPadding ->
         Column(
             modifier = Modifier

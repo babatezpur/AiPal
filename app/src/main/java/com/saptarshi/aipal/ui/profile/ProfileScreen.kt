db1cda97269b70f1e514a11d4fba8f0daec001a4
Saptarshi <babatezpur@gmail.com>
2026-03-19 20:10:18 +0530

    Implement ProfileInfo section in ProfileScreen

--- app/src/main/java/com/saptarshi/aipal/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/saptarshi/aipal/ui/profile/ProfileScreen.kt
@@ -5,13 +5,16 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CameraAlt
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -25,8 +28,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.saptarshi.aipal.R
 
@@ -58,7 +63,8 @@ fun ProfileViewModelContent(
 ) {
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(20.dp)
     ) {
@@ -73,8 +79,61 @@ fun ProfileViewModelContent(
         )
 
         ProfilePicture(imgPath)
-//        ProfileInfo(name, email)
+        ProfileInfo(name, email ) { newName ->
+            onNameChange(newName)
+        }
 
+    }
+}
+
+fun onNameChange(newName: String) {
+    //
+}
+
+@Composable
+fun ProfileInfo(name: String, email: String, onNameChange: (newName : String) -> Unit) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "Name",
+            style = MaterialTheme.typography.bodyLarge,
+            // bold the text
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(end = 30.dp, bottom = 20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Text(
+                text = name,
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold
+            )
+            Icon(
+                imageVector = Icons.Filled.Edit,
+                contentDescription = "Change name",
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.clickable {
+                    onNameChange(name)
+                }
+            )
+        }
+        Text(
+            text = "Email",
+            style = MaterialTheme.typography.bodyLarge,
+
+        )
+        Text(
+            text = email,
+            style = MaterialTheme.typography.bodyLarge ,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
@@ -115,7 +174,7 @@ fun ProfilePicture(imgPath: String, onClick: () -> Unit = {}) {
 fun ProfileScreenPreview() {
     ProfileViewModelContent(
         name = "Saptarshi",
-        email = "william.henry.harrison@example-pet-store.com",
+        email = "william.harrison@example-pet-store.com",
         imgPath = ""
     )
 }
